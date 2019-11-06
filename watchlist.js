87cbbaec1c3f1c3869b8381bad31e7cda441bca1
De Mclen <Desiree1@Desirees-MacBook-Air.local>
2019-11-05 20:11:38 -0600

    working code

--- watchlist.js
+++ watchlist.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/* document.addEventListener('DOMContentLoaded', function() {
 function displayMovie(savedMovie) {
     var watchlistHTML = savedMovie.map(function(watchlistMovie){
         return `
@@ -39,7 +39,36 @@ function myWatchlist(imdbID) {
     watchlist = [];
     watchlist.push(movie);
     watchlistJSON = JSON.stringify(watchlist);
-    localStorage.setItem('watchlist', watchlistJSON) */
+    localStorage.setItem('watchlist', watchlistJSON) 
    
 
-}
+} */
+document.addEventListener('DOMContentLoaded', function() {
+    var watchListJSON = localStorage.getItem("watchlist");
+    var watchlist = JSON.parse(watchListJSON);
+    console.log(watchlist);
+
+  function renderMovies(movieArray) {
+    var movieHTML = movieArray.map(function (currentMovie){
+      return `
+          <div class="movie">
+							<div class="card" style="width: 18rem;">
+									<img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">${currentMovie.Title} <span class="badge badge-secondary">${currentMovie.Year}</span></h5>
+										<p class="card-text">IMDB ID: ${currentMovie.imdbID}</p>
+										<button onclick="saveToWatchlist('${currentMovie.imdbID}')" class="btn btn-primary">Go somewhere</button>
+									</div>
+						</div>      
+									</div>
+							</div>
+          </div>
+          `;
+    });
+
+    return movieHTML.join('');
+  }
+
+  var content = document.getElementById('movies-container');
+  content.innerHTML = renderMovies(watchlist);
+});
